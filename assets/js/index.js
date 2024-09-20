badf7548e7e5525c2a132c9b906dceb2c19af12d
unknown <sajedislam019@gmail.com>
2024-09-20 23:48:59 +0600

    javascript and new file added

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+ 
+
+  const mobile_nav = document.querySelector(".mobile-navbar-btn");
+
+  const nav_header = document.querySelector(".header-top")
+
+  const toggleNavbar = () => {
+    nav_header.classList.toggle("active")
+  }
+
+  mobile_nav.addEventListener("click", () => toggleNavbar());
+  
+
+
+
+  
